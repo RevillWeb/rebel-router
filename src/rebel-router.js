12a56b3c957911b3abceec0e5d522a97bddba514
Leon Revill <leon@revillwebdesign.com>
2016-02-28 16:19:47 +0000

    Update repository to use NPM

--- src/rebel-router.js
+++ src/rebel-router.js
@@ -1,11 +1,9 @@
 /**
  * Created by Leon Revill on 15/12/2015.
- * Blog: http://www.revilweb.com
+ * Blog: http://www.jsinsights.com
  * GitHub: https://github.com/RevillWeb
  * Twitter: @RevillWeb
  */
-
-import * as RebelCore from 'rebel-core';
 
 function _routeResult(templateName, route, regex, path) {
     let result = {};
@@ -63,9 +61,45 @@ export class RebelRouter extends HTMLElement {
     setDefault(ViewClass) {
         this.add("*", ViewClass);
     }
+    static parseQueryString(url) {
+        var result = {};
+        if (url !== undefined) {
+            var queryString = (url.indexOf("?") > -1) ? url.substr(url.indexOf("?") + 1, url.length) : null;
+            if (queryString !== null) {
+                queryString.split("&").forEach(function (part) {
+                    if (!part) return;
+                    part = part.replace("+", " ");
+                    var eq = part.indexOf("=");
+                    var key = eq > -1 ? part.substr(0, eq) : part;
+                    var val = eq > -1 ? decodeURIComponent(part.substr(eq + 1)) : "";
+                    var from = key.indexOf("[");
+                    if (from == -1) result[decodeURIComponent(key)] = val;
+                    else {
+                        var to = key.indexOf("]");
+                        var index = decodeURIComponent(key.substring(from + 1, to));
+                        key = decodeURIComponent(key.substring(0, from));
+                        if (!result[key]) result[key] = [];
+                        if (!index) result[key].push(val);
+                        else result[key][index] = val;
+                    }
+                });
+            }
+        }
+        return result;
+    }
+    static classToTag(Class) {
+        var name = Class.name.replace(/\W+/g, '-').replace(/([a-z\d])([A-Z0-9])/g, '$1-$2').toLowerCase();
+        if (validElementTag(name) === false) {
+            throw new Error("Class name couldn't be translated to tag.");
+        }
+        return name;
+    }
+    static isRegisteredElement(name) {
+        return document.createElement(name).constructor !== HTMLElement;
+    }
     static create(Class) {
-        const name = RebelCore.classToTag(Class);
-        if (RebelCore.isRegisteredElement(name) === false) {
+        const name = RebelRouter.classToTag(Class);
+        if (RebelRouter.isRegisteredElement(name) === false) {
             document.registerElement(name, Class);
         }
         return name;
@@ -91,7 +125,7 @@ export class RebelRouter extends HTMLElement {
         }
     }
     static getParamsFromUrl(regex, route, path) {
-        var result = RebelCore.parseQueryString(path);
+        var result = RebelRouter.parseQueryString(path);
         var re = /{(\w+)}/g;
         var results = [];
         let match;
